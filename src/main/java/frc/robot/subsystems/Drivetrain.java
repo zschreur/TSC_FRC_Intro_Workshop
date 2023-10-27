e7e172e6d12bc13feb4302a26ea132011ab679b0
Zackary Schreur <z.schreur@techsmith.com>
2023-10-27 08:27:35 -0400

    Add the differential drive

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,6 +9,9 @@ public class Drivetrain extends SubsystemBase {
     // PWM channels 0 and 1 respectively
     private final Spark m_leftMotor = new Spark(0);
     private final Spark m_rightMotor = new Spark(1);
+
+    // Set up the differential drive controller
+    private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
 
     public Drivetrain() {
         m_rightMotor.setInverted(true);
