2eb08c506ebf48a097e321a458b18c21110a7ea2
Zackary Schreur <z.schreur@techsmith.com>
2023-10-27 08:27:21 -0400

    Add the left & right motor to the subsystem

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,15 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Drivetrain extends SubsystemBase {}
+public class Drivetrain extends SubsystemBase {
+    // The Romi has the left and right motors set to
+    // PWM channels 0 and 1 respectively
+    private final Spark m_leftMotor = new Spark(0);
+    private final Spark m_rightMotor = new Spark(1);
+
+    public Drivetrain() {
+        m_rightMotor.setInverted(true);
+    }
+}
